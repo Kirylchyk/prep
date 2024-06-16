76e1c822cb3efc582f96d9f9baa7b71e4913070a
varvara <kirylchyk@gmail.com>
2024-06-16 18:18:57 -0400

    cleanup

--- Class-Object/SimpleObjectClass.js
+++ Class-Object/SimpleObjectClass.js
@@ -1,3 +1,5 @@
+//JS
+
 class Person {
     constructor(name, age) {
         this.name = name;
@@ -14,3 +16,26 @@ person1.greet(); // Outputs: Hello, my name is Alice, and I am 30 years old.
 
 let person2 = new Person("Bob", 25);
 person2.greet(); // Outputs: Hello, my name is Bob, and I am 25 years old.
+
+//TS
+
+class PersonTS {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet(): void {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
+
+let person3 = new PersonTS("Alice", 30);
+person1.greet(); // Outputs: Hello, my name is Alice, and I am 30 years old.
+
+let person4 = new PersonTS("Bob", 25);
+person2.greet(); // Outputs: Hello, my name is Bob, and I am 25 years old.
+
