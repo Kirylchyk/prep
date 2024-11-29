cbac3f3cef5ed6e409e6b1d82934b02f31226023
Varvara <kieylchyk@gmail.com>
2024-11-29 17:27:23 -0500

    TypeMart

---
+++ TypeMart/index.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var products_1 = require("./products");
+var productName = 'beanie';
+var shipping;
+var taxPercent;
+var taxTotal;
+var total;
+var shippingAddress = 'Nice Place, Nice Country, 0001';
+var product = products_1.default.filter(function (product) { return product.name === productName; })[0];
+if (String(product.preOrder) === 'true') {
+    console.log('We will send you a message when your product is on its way.');
+}
+if (Number(product.price) > 25) {
+    shipping = 0;
+}
+else {
+    shipping = 5;
+    console.log("We provide free shipping for this product.");
+}
+if (shippingAddress.match('001')) {
+    taxPercent = 0.1;
+}
+else {
+    taxPercent = 0.05;
+}
+taxTotal = Number(product.price) * taxPercent;
+total = Number(product.price) + taxTotal + shipping;
+console.log(product);
+console.log("Your receipt:\nProduct:  ".concat(product.name, "\nAddress:  ").concat(shippingAddress, "\nPrice:    $").concat(product.price, "\nTax:      $").concat(taxTotal.toFixed(2), "\nShipping: $").concat(shipping.toFixed(2), "\nTotal:    $").concat(total.toFixed(2), "\n"));
---
+++ TypeMart/index.ts
@@ -0,0 +1,47 @@
+import products from './products';
+const productName: string = 'beanie';
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
+let shippingAddress:string = 'Nice Place, Nice Country, 0001';
+
+
+const product = products.filter(product => product.name === productName)[0];
+
+if (String(product.preOrder) === 'true') {
+  console.log('We will send you a message when your product is on its way.');
+}
+
+if (Number(product.price) > 25){
+  shipping = 0; 
+} else {
+  shipping = 5;
+  console.log ("We provide free shipping for this product.")
+}
+
+if (shippingAddress.match('001')) {
+  taxPercent = 0.1;
+} else {
+  taxPercent = 0.05;
+}
+
+taxTotal = Number(product.price) * taxPercent;
+
+total = Number(product.price) + taxTotal + shipping; 
+
+
+console.log(product);
+console.log(`Your receipt:
+Product:  ${product.name}
+Address:  ${shippingAddress}
+Price:    $${product.price}
+Tax:      $${taxTotal.toFixed(2)}
+Shipping: $${shipping.toFixed(2)}
+Total:    $${total.toFixed(2)}
+`);
+
+
+
+
+
