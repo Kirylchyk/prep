16f215019e95d80827643360f88e7e3617aea759
Varvara <kirylchyk@gmail.com>
2024-12-01 20:23:45 -0500

    typeof01

--- challenges/arrays.ts
+++ challenges/arrays.ts
@@ -8,3 +8,14 @@ let bestBooleans: boolean[] = [true, false];
 let bestMealPlan: string[][] = [bestLunches, bestBreakfasts, ['baked potato', 'mashed potato']];
 let bestBooleansTwice: boolean[][] = [bestBooleans, bestBooleans];
 let numbersMulti: number[][][] = [ [[1],[2,3]], [[7],bestNumbers] ];
+
+
+// tuple =  array with elements of specific types
+
+let favoriteCoordinates:[number, number, string, number, number, string] = [17, 45, 'N', 142, 30, 'E'];
+
+let tup: [string, string] = ['hi', 'bye'];
+let arr: string[] = ['there','there'];
+tup = ['there', 'there']; // No Errors.
+//tup = arr; // Type Error! An array cannot be assigned to a tuple.
+
